44f1da05432e571d5fed9680a0e3127e641fb979
Enterprise2364 <joanna.senn@students.fhnw.ch>
2023-05-06 22:15:30 +0200

    Maschine Learning Modell komplett

---
+++ PFD/Object_Detection/model_downloader.py
@@ -0,0 +1,9 @@
+import wget
+import tarfile
+
+model_link = "http://download.tensorflow.org/models/object_detection/tf2/20200711/efficientdet_d0_coco17_tpu-32.tar.gz"
+wget.download(model_link)
+
+tar = tarfile.open("efficientdet_d0_coco17_tpu-32.tar.gz")
+tar.extractall(".")
+tar.close()
