d9cec89d76243519787a0a036b471aff1ac5cc09
Msophys1 <matteo.sophys@students.fhnw.ch>
2023-05-15 20:20:46 +0200

    Implementierung von absoluten Pfad

--- PFD/PFD.bat
+++ PFD/PFD.bat
@@ -1,9 +1,9 @@
 @echo off
 REM Erstes Python-Skript ausführen
-"C:\Users\widde\anaconda3\envs\tf2\python.exe" C:\Users\widde\Desktop\PFD\Object_Detection\Detect_images.py -m C:\Users\widde\Desktop\PFD\Object_Detection\inference_graph\saved_model -l C:\Users\widde\Desktop\PFD\Object_Detection\labelmap.pbtxt -i C:\Users\widde\Desktop\PFD\Object_Detection\test_images
+"C:\Users\33782\AppData\Local\Programs\Python\Python311\python.exe" D:\tello\PFD\Object_Detection\Detect_images.py -m D:\tello\PFD\Object_Detection\inference_graph\saved_model -l D:\tello\PFD\Object_Detection\labelmap.pbtxt -i D:\tello\Images
 
 REM Zweites Python-Skript ausführen
-"C:\Users\widde\anaconda3\envs\tf2\python.exe" "C:\Users\widde\Desktop\PFD\Object_Detection\open_img.py"
+"C:\Users\33782\AppData\Local\Programs\Python\Python311\python.exe" "D:\tello\PFD\Object_Detection\open_img.py"
 
 REM Batch-Skript beenden
 pause
